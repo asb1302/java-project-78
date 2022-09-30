5db55607d3f83090697e930226d98879787e412a
Alexey Beliy <bas95work@gmail.com>
2022-09-30 10:19:16 +0300

    finish step 5

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,7 +1,10 @@
 package hexlet.code.schemas;
 
 import hexlet.code.constraints.IsMap;
+import hexlet.code.constraints.Shape;
 import hexlet.code.constraints.SizeOf;
+
+import java.util.Map;
 
 public final class MapSchema extends BaseSchema implements Schema {
     public MapSchema() {
@@ -10,6 +13,12 @@ public final class MapSchema extends BaseSchema implements Schema {
 
     public MapSchema sizeof(int size) {
         this.addConstraint(new SizeOf(size));
+
+        return this;
+    }
+
+    public MapSchema shape(Map<String, BaseSchema> schemas) {
+        this.addConstraint(new Shape(schemas));
 
         return this;
     }
--- src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -9,6 +9,8 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MapSchemaTest {
+    private static final int NUMBER_1 = 100;
+    private static final int NUMBER_2 = -5;
     @Test
     void testValidate() {
         Validator v = new Validator();
@@ -31,5 +33,40 @@ public class MapSchemaTest {
         data.put("key2", "value2");
 
         assertThat(schema.isValid(data)).isEqualTo(true);
+    }
+
+    @Test
+    void testValidateShape() {
+        Validator v = new Validator();
+
+        MapSchema schema = v.map();
+
+        // shape - позволяет описывать валидацию для значений объекта Map по ключам.
+        Map<String, BaseSchema> schemas = new HashMap<>();
+
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", NUMBER_1);
+        assertThat(schema.isValid(human1)).isEqualTo(true);
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertThat(schema.isValid(human2)).isEqualTo(true);
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertThat(schema.isValid(human3)).isEqualTo(false);
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", NUMBER_2);
+        assertThat(schema.isValid(human4)).isEqualTo(false);
     }
 }
